69ce3f90a2b1bde66652572b87ba38c792fd3f5f
RMKendall <ryan.morris.kendall@gmail.com>
2020-10-14 11:31:10 -0500

    Save second half of Annotation exercise

--- src/Author.java
+++ src/Author.java
@@ -5,22 +5,19 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Author extends Person {
-    private List books;
+    private ArrayList<String> books;
 
     public Author(String firstName, String lastName) {
         super(firstName, lastName);
-        books = new ArrayList();
+        books = new ArrayList<>();
     }
 
-    /**
-     * @deprecated Use publishedBooks instead
-     */
-    @Deprecated
-    public List<String> getBooks() {
+
+    public ArrayList<String> getBooks() {
         return books;
     }
 
-    public List<String> publishedBooks() {
+    public ArrayList<String> publishedBooks() {
         return books;
     }
 
@@ -29,7 +26,7 @@ public class Author extends Person {
     }
 
     @Override
-    public String sortName() {
+    public String fullName() {
         return String.format("%s, %s", lastName, firstName);
     }
 }
